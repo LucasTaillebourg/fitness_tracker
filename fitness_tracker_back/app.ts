114eb8079a962cd4b872e304d3c90726c0c36c7a
lucastaillebourg <lucas.taillebourg@gmail.com>
2023-11-16 00:05:21 +0100

    [Back] Add eslint

--- fitness_tracker_back/app.ts
+++ fitness_tracker_back/app.ts
@@ -1,46 +1,45 @@
-import express from 'express';
-import * as userController from './app/features/userController';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import express from 'express'
+import * as userController from './app/features/userController'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
-const app = express();
-const port = 3000;
-dotenv.config();
+const app = express()
+const port = 3000
+dotenv.config()
 
-app.use(express.json());
+app.use(express.json())
 
 // Endpoint pour l'inscription (création d'un utilisateur)
-app.post('/api/register', userController.registerUser);
+app.post('/api/register', userController.registerUser)
 
 // Endpoint pour la connexion
-app.post('/api/login', userController.loginUser);
+app.post('/api/login', userController.loginUser)
 
 // Endpoint pour récupérer les informations de l'utilisateur (exigeant un token valide)
-app.get('/api/user', authenticateUser, userController.getUsers);
+app.get('/api/user', authenticateUser, userController.getUsers)
 
-const secretKey = process.env.SECRET_KEY ?? ""
-
+const secretKey = process.env.SECRET_KEY ?? ''
 
 // Fonction middleware pour l'authentification
-function authenticateUser(req: any, res: any, next: any) {
+function authenticateUser (req: any, res: any, next: any) {
   // Vérifier la présence du token dans l'en-tête Authorization
-  const token = req.header('Authorization');
+  const token = req.header('Authorization')
 
   if (!token) {
-    return res.status(401).json({ error: 'Unauthorized - Missing token' });
+    return res.status(401).json({ error: 'Unauthorized - Missing token' })
   }
 
   try {
     // Vérifier la validité du token
-    const decoded = jwt.verify(token, secretKey); // Remplacez par votre clé secrète
-    req.userId = typeof decoded === 'string' ? decoded : decoded.userId; // Stocker l'ID utilisateur dans la requête
-    next();
+    const decoded = jwt.verify(token, secretKey) // Remplacez par votre clé secrète
+    req.userId = typeof decoded === 'string' ? decoded : decoded.userId // Stocker l'ID utilisateur dans la requête
+    next()
   } catch (error) {
-    console.error('Erreur de vérification du token:', error);
-    res.status(401).json({ error: 'Unauthorized - Invalid token' });
+    console.error('Erreur de vérification du token:', error)
+    res.status(401).json({ error: 'Unauthorized - Invalid token' })
   }
 }
 
 app.listen(port, () => {
-  console.log(`Serveur en cours d'exécution sur le port ${port}`);
-});
+  console.log(`Serveur en cours d'exécution sur le port ${port}`)
+})
--- fitness_tracker_back/app/features/userController.ts
+++ fitness_tracker_back/app/features/userController.ts
@@ -1,99 +1,98 @@
-import { Request, Response } from 'express';
-import * as db from './db';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-
+import { type Request, type Response } from 'express'
+import * as db from './db'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export const registerUser = async (req: Request, res: Response) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body
 
-  const userExists = await checkIfUserExists(username);
+  const userExists = await checkIfUserExists(username)
   if (userExists) {
-    return res.status(400).json({ error: 'Username already exists' });
+    return res.status(400).json({ error: 'Username already exists' })
   }
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10)
 
-  const insertQuery = 'INSERT INTO FITNESS_USER (username, password_hash) VALUES ($1, $2) RETURNING *';
-  const values = [username, hashedPassword];
+  const insertQuery = 'INSERT INTO FITNESS_USER (username, password_hash) VALUES ($1, $2) RETURNING *'
+  const values = [username, hashedPassword]
 
   try {
-    const result = await db.query(insertQuery, values);
-    const newUser = result.rows[0];
+    const result = await db.query(insertQuery, values)
+    const newUser = result.rows[0]
 
     // Générer un token d'authentification
-    const token = generateToken(newUser.id);
+    const token = generateToken(newUser.id)
 
     // Mettre à jour la base de données avec le token
-    await updateUserToken(newUser.id, token);
+    await updateUserToken(newUser.id, token)
 
-    res.status(201).json({ user: newUser, token });
+    res.status(201).json({ user: newUser, token })
   } catch (error) {
-    console.error('Erreur lors de l\'inscription:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error('Erreur lors de l\'inscription:', error)
+    res.status(500).json({ error: 'Internal server error' })
   }
-};
+}
 
 // Endpoint pour la connexion
 export const loginUser = async (req: Request, res: Response) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body
 
   // Vérifier si l'utilisateur existe
-  const user = await getUserByUsername(username);
+  const user = await getUserByUsername(username)
   if (!user) {
-    return res.status(401).json({ error: 'Invalid username or password' });
+    return res.status(401).json({ error: 'Invalid username or password' })
   }
 
   // Vérifier le mot de passe
-  const isPasswordValid = await bcrypt.compare(password, user.password_hash);
+  const isPasswordValid = await bcrypt.compare(password, user.password_hash)
   if (!isPasswordValid) {
-    return res.status(401).json({ error: 'Invalid username or password' });
+    return res.status(401).json({ error: 'Invalid username or password' })
   }
 
   // Générer un token d'authentification
-  const token = generateToken(user.id);
+  const token = generateToken(user.id)
 
   // Mettre à jour la base de données avec le token
-  await updateUserToken(user.id, token);
+  await updateUserToken(user.id, token)
 
-  res.json({ user, token });
-};
+  res.json({ user, token })
+}
 
 export const getUsers = async (req: Request, res: Response) => {
   try {
-    const query = 'SELECT * FROM FITNESS_USER';
-    const result = await db.query(query);
+    const query = 'SELECT * FROM FITNESS_USER'
+    const result = await db.query(query)
 
-    res.json(result.rows);
+    res.json(result.rows)
   } catch (error) {
-    console.error('Erreur lors de la récupération des utilisateurs:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error('Erreur lors de la récupération des utilisateurs:', error)
+    res.status(500).json({ error: 'Internal server error' })
   }
-};
+}
 
 // Fonction pour vérifier si un utilisateur existe déjà
 const checkIfUserExists = async (username: string) => {
-  const query = 'SELECT * FROM FITNESS_USER WHERE username = $1';
-  const result = await db.query(query, [username]);
-  return result.rows.length > 0;
-};
+  const query = 'SELECT * FROM FITNESS_USER WHERE username = $1'
+  const result = await db.query(query, [username])
+  return result.rows.length > 0
+}
 
 // Fonction pour récupérer un utilisateur par nom d'utilisateur
 const getUserByUsername = async (username: string) => {
-  const query = 'SELECT * FROM FITNESS_USER WHERE username = $1';
-  const result = await db.query(query, [username]);
-  return result.rows[0];
-};
+  const query = 'SELECT * FROM FITNESS_USER WHERE username = $1'
+  const result = await db.query(query, [username])
+  return result.rows[0]
+}
 
 // Fonction pour générer un token d'authentification
 const generateToken = (userId: number) => {
-  const secretKey = process.env.SECRET_KEY ?? ""
-  return jwt.sign({ userId }, secretKey, { expiresIn: '1h' });
-};
+  const secretKey = process.env.SECRET_KEY ?? ''
+  return jwt.sign({ userId }, secretKey, { expiresIn: '1h' })
+}
 
 // Fonction pour mettre à jour le token dans la base de données
 const updateUserToken = async (userId: number, token: string) => {
-  const updateQuery = 'UPDATE FITNESS_USER SET token = $1 WHERE id = $2';
-  const values = [token, userId];
-  await db.query(updateQuery, values);
-};
+  const updateQuery = 'UPDATE FITNESS_USER SET token = $1 WHERE id = $2'
+  const values = [token, userId]
+  await db.query(updateQuery, values)
+}
