0469c661f6f2b73aae2a5a85dfe948e37a8566c8
lucastaillebourg <lucas.taillebourg@gmail.com>
2023-11-18 20:31:35 +0100

    [Front] Setup eslint

--- fitness_tracker_front/.eslintrc.cjs
+++ fitness_tracker_front/.eslintrc.cjs
@@ -1,18 +1,27 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+    '@typescript-eslint',
+    'prettier',
+  ],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
